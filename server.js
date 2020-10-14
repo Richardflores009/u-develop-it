d01b6c8e9eaa9d4c8fbdf135a978944d34d9062e
Richy Rich <richardflores009@gmail.com>
2020-10-14 11:55:17 -0700

    added join statements for id and all candidates routes

--- server.js
+++ server.js
@@ -25,8 +25,13 @@ app.get('/', (req, res) => {
   });
 
 // Get all candidates
-app.get('/api/candidate', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+app.get('/api/candidates', (req, res) => {
+  const sql = 
+  `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
       if (err) {
@@ -43,8 +48,13 @@ app.get('/api/candidate', (req, res) => {
 
 // Get single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates 
-                 WHERE id = ?`;
+  const sql = 
+  `SELECT candidates.*, parties.name 
+  AS party_name 
+  FROM candidates 
+  LEFT JOIN parties 
+  ON candidates.party_id = parties.id 
+  WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
       if (err) {
